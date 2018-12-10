932ff35178c7c1dd5ba7cd296410e2de12b62ac1
Mikal Morello <mikalmorello@gmail.com>
2018-12-09 21:26:44 -0500

    JS - add result count, reset results on submit, submit via keyboard

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-console.log('ready');
-
 // VARIABLES
 const xhr = new XMLHttpRequest(),
       baseUrl = 'https://api.edamam.com/search?q=',
@@ -15,14 +13,14 @@ const xhr = new XMLHttpRequest(),
 let searchResultRange = 9;
     searchResultMin = 0,
     searchResultMax = searchResultMin + searchResultRange,
-    searchInputValue = '';
+    searchInputValue = '',
+    searchCount = 0,
+    isSearchNew = '';
 
 // FUNCTIONS
 
 // API Call
-function callThatAPI(searchParams) {
-  console.log('Call Search Result Min' + searchResultMin);
-  console.log('Call Search Result Max' + searchResultMax);
+function callThatAPI(searchParams, isSearchNew) {
   xhr.open('GET', `${baseUrl}${searchParams}${appId}${appKey}&from=${searchResultMin}&to=${searchResultMax}`);
   xhr.send();
   xhr.onload = handleSuccess;
@@ -31,21 +29,27 @@ function callThatAPI(searchParams) {
 
 // API Success
 function handleSuccess() {
+  searchCount = searchCount + 1;
   var response = JSON.parse(xhr.responseText);
   console.log(response);
-  var hits = response.hits;
-  for(let i = 0; i < hits.length; i++) {
-   console.log('is this working');
-   resultContainer.innerHTML += `<article class="edamam__card"><img src="${hits[i].recipe.image}">${hits[i].recipe.label}</article>` 
-  }
-  loadMoreButton.classList.add('load-more--show');
-  header.classList.remove('header--full-screen');
+  loadResults(response);
   resultCount();
+  if (searchCount === 1) {
+    loadMoreButton.classList.add('load-more--show');
+    header.classList.remove('header--full-screen');
+    resultCountContainer.classList.add('edamam-results--show');
+    console.log('first load');
+  } else {
+    console.log('subsequent load');
+  }
 }
 
 // API Error
 function handleError() {
   console.log('oops');
+  resetResults();
+  resultCountContainer.classList.remove('edamam-results--show');
+  loadMoreButton.classList.remove('load-more--show');
 }
 
 // Inifnite Load count
@@ -55,27 +59,63 @@ function infiniteLoad(currentResults) {
   searchResultMax = searchResultMin + searchResultRange;
 }
 
-// Inifnite Load count
+// Result Count
 
 function resultCount() {
-  console.log('Result count is ' + searchResultMax);
-  resultCountContainer.innerHTML += `Result count is ${searchResultMax}`;          
+  resultCountContainer.innerHTML = `Result count is ${searchResultMax}`;          
 }
 
-// Layout testing
+// Load Results
+
+function loadResults(response){
+  var hits = response.hits;
+  for(let i = 0; i < hits.length; i++) {
+   resultContainer.innerHTML += `<article class="edamam__card"><img src="${hits[i].recipe.image}">${hits[i].recipe.label}</article>` 
+  }
+}
+
+// Reset results
+
+function resetResults() {
+  resultContainer.innerHTML = '';
+  searchResultMin = 0;
+  searchResultMax = searchResultMin + searchResultRange;
+}
+
+// Check if new search term
+
+function checkSearchTerm (newSearchInput){
+  if(searchInputValue === newSearchInput){
+    return isSearchNew = false;
+  } else {
+    return isSearchNew = true;
+  }
+}
+
+// Autorun testing if you don't want to render a search
 /*function autoRun(searchValue) {
   callThatAPI(searchValue);
 }*/
-
 
 //autoRun('carrots');
 
 // EVENT LISTENER
 
+// Submit message if user hits enter on input
+searchInput.addEventListener('keyup', function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    searchButton.click();
+  }
+});
+
+
 // Search Button Click
 searchButton.addEventListener('click', function() {
   event.preventDefault();
-  callThatAPI(searchInput.value);
+  checkSearchTerm(searchInput.value);
+  resetResults();
+  callThatAPI(searchInput.value, isSearchNew);
   searchInputValue = searchInput.value;
   return searchInputValue;
 });
@@ -84,6 +124,5 @@ searchButton.addEventListener('click', function() {
 loadMoreButton.addEventListener('click', function() {
   event.preventDefault();
   infiniteLoad(searchResultMax);
-  console.log(searchInputValue);
   callThatAPI(searchInputValue);
 });
